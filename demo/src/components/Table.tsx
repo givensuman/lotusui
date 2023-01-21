5a731697e002e69c5a14ccb7be9eac18e1f4ea79
Given Suman <givensuman@gmail.com>
2023-01-20 18:50:30 -0600

    Improve table

--- demo/src/components/Table.tsx
+++ demo/src/components/Table.tsx
@@ -35,6 +35,40 @@ const Table = () => {
                     </tr>
                 </tfoot>
             </table>
+            <table className="table mt-8 table-lg table-striped">
+                <caption>Imperial to metric conversion factors</caption>
+                <thead>
+                    <tr>
+                        <th>To convert</th>
+                        <th>into</th>
+                        <th>multiply by</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>inches</td>
+                        <td>millimetres (mm)</td>
+                        <td>25.4</td>
+                    </tr>
+                    <tr>
+                        <td>feet</td>
+                        <td>centimetres (cm)</td>
+                        <td>30.48</td>
+                    </tr>
+                    <tr>
+                        <td>yards</td>
+                        <td>metres (m)</td>
+                        <td>0.91444</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>To convert</th>
+                        <th>into</th>
+                        <th>multiply by</th>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     )
 }
