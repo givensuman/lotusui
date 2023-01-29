6d20e836626f27b963ccb99a1d5422dfc204aaf8
Given Suman <givensuman@gmail.com>
2023-01-29 12:19:38 -0600

    Convert radio to bulma syntax

--- fixtures/src/Radio.tsx
+++ fixtures/src/Radio.tsx
@@ -12,36 +12,36 @@ export default function Radio() {
             <div>
                 <h1>Color Variants</h1>
                 <div className="flex space-x-2">
-                    <input type="radio" className="radio radio-slate" />
-                    <input type="radio" className="radio radio-gray" />
-                    <input type="radio" className="radio radio-zinc" />
-                    <input type="radio" className="radio radio-neutral" />
-                    <input type="radio" className="radio radio-stone" />
-                    <input type="radio" className="radio radio-red" />
-                    <input type="radio" className="radio radio-orange" />
-                    <input type="radio" className="radio radio-amber" />
-                    <input type="radio" className="radio radio-yellow" />
-                    <input type="radio" className="radio radio-lime" />
-                    <input type="radio" className="radio radio-green" />
-                    <input type="radio" className="radio radio-emerald" />
-                    <input type="radio" className="radio radio-teal" />
-                    <input type="radio" className="radio radio-cyan" />
-                    <input type="radio" className="radio radio-sky" />
-                    <input type="radio" className="radio radio-blue" />
-                    <input type="radio" className="radio radio-indigo" />
-                    <input type="radio" className="radio radio-violet" />
-                    <input type="radio" className="radio radio-purple" />
-                    <input type="radio" className="radio radio-fuchsia" />
-                    <input type="radio" className="radio radio-pink" />
-                    <input type="radio" className="radio radio-rose" />
+                    <input type="radio" className="radio is-slate" />
+                    <input type="radio" className="radio is-gray" />
+                    <input type="radio" className="radio is-zinc" />
+                    <input type="radio" className="radio is-neutral" />
+                    <input type="radio" className="radio is-stone" />
+                    <input type="radio" className="radio is-red" />
+                    <input type="radio" className="radio is-orange" />
+                    <input type="radio" className="radio is-amber" />
+                    <input type="radio" className="radio is-yellow" />
+                    <input type="radio" className="radio is-lime" />
+                    <input type="radio" className="radio is-green" />
+                    <input type="radio" className="radio is-emerald" />
+                    <input type="radio" className="radio is-teal" />
+                    <input type="radio" className="radio is-cyan" />
+                    <input type="radio" className="radio is-sky" />
+                    <input type="radio" className="radio is-blue" />
+                    <input type="radio" className="radio is-indigo" />
+                    <input type="radio" className="radio is-violet" />
+                    <input type="radio" className="radio is-purple" />
+                    <input type="radio" className="radio is-fuchsia" />
+                    <input type="radio" className="radio is-pink" />
+                    <input type="radio" className="radio is-rose" />
                 </div>
             </div>
             <div>
                 <h1>Size Variants</h1>
                 <div className="flex space-x-2">
-                    <input type="radio" className="radio radio-sm" />
+                    <input type="radio" className="radio is-sm" />
                     <input type="radio" className="radio" />
-                    <input type="radio" className="radio radio-lg" />
+                    <input type="radio" className="radio is-lg" />
                 </div>
             </div>
             <div>
